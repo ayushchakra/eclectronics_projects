e83b714617b1ca282414a79de8538c2c3b1277b8
ayushchakra <achakraborty@olin.edu>
2022-09-15 02:13:20 -0400

    updated ltspice analysis and uploaded files

--- mini_project_one/ltspice/ltspice_python_analysis/mp1_data_analysis.py
+++ mini_project_one/ltspice/ltspice_python_analysis/mp1_data_analysis.py
@@ -1,17 +1,11 @@
 import pdb
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-file_name = 'wca_data.txt'
+FILE_NAME = f'{os.getcwd()}/ltspice/ltspice_data/refined_sweep.txt'
 
-def plot():
-    plt.plot(times, v_values)
-    plt.axis('equal')
-    plt.ylim(0, 3.3)
-    plt.show()
-
-
-def find_period(times, v_values):
+def find_period(times, v_values, current_resistor):
     index = 0
     t_val = times[0]
     v_val = v_values[0]
@@ -26,31 +20,40 @@ def find_period(times, v_values):
     while v_val < 1.65:
         index += 1
         v_val = v_values[index]
+    while v_val > 1.65:
+        index += 1
+        v_val = v_values[index]
     time_high = times[index]
-    period = (time_high-time_low)*2
-    # print(f'Period: {(time_high-time_low)*2}s')
+    period = (time_high-time_low)
+    print(f'Period of {current_resistor} is: {period}s')
     return period
 
-periods = []
+def main():
+    periods = []
 
-with open(file_name, 'r') as txt_file:
-    lines = txt_file.readlines()
-    index = 0
-    while index < len(lines):
-        times = np.array([])
-        v_values = np.array([])
-        if lines[index][0] == 't' or lines[index][0] == 'S':
-            index += 1
-            continue
-        while index < len(lines) and lines[index][0] != 'S':
-            current_list = lines[index].split()
-            times = np.append(times, float(current_list[0]))
-            v_values = np.append(v_values, float(current_list[1]))
-            index += 1
-        periods.append(find_period(times, v_values))
+    with open(FILE_NAME, 'r') as txt_file:
+        lines = txt_file.readlines()
+        index = 0
+        current_resistor = ''
+        while index < len(lines):
+            times = np.array([])
+            v_values = np.array([])
+            if lines[index][0] == 't':
+                index += 1
+                continue
+            if lines[index][0] == 'S':
+                current_resistor = lines[index][18:-15]
+                index += 1
+                continue
+            while index < len(lines) and lines[index][0] != 'S':
+                current_list = lines[index].split()
+                times = np.append(times, float(current_list[0]))
+                v_values = np.append(v_values, float(current_list[1]))
+                index += 1
+            periods.append(find_period(times, v_values, current_resistor))
 
-max_period = np.max(periods)
-min_period = np.min(periods)
+    # print(f'Max Period is {np.max(periods)}s')
+    # print(f'Min Period is {np.min(periods)}s')
 
-print(f'Max Period is {max_period}s')
-print(f'Min Period is {min_period}s')
+if __name__ == '__main__':
+    main()
